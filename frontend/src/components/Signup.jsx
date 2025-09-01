72b5b6e6e4e97908e046c76d27d3a38b5a9ffc20
Karthik2324B <karthik903294@gmail.com>
2025-09-01 17:01:27 +0530

    Update Signup.jsx added backend url

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -15,7 +15,7 @@ const Signup = () => {
       email:data.email,
       password:data.password
     }
-    await axios.post('http://localhost:4001/user/signup',userInfo).then((res)=>{
+    await axios.post('https://bookstore-backend-9kv7.onrender.com/user/signup',userInfo).then((res)=>{
        console.log(res)
        if(res.data){
         toast.success("Signup Successful")
